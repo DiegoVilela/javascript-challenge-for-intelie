ec54b6d32923fd6b174f3fca8642a904480293cb
Diego Vilela <vilelaphp@gmail.com>
2022-05-28 17:32:47 -0300

    Added feature to process input text into a list of event objects

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import { events } from './data.js';
 import { browserValues, createDatasets, getDataEvents, osValues } from './utils.js';
 
+const EVENTS_TEXTAREA_ID = 'eventsSequence';
 const CHART_ID = 'browsersResponseTimeChart';
+const BUTTON_ID = 'submitButton';
 
 const labels = ['00:00', '00:01'];
 
@@ -22,9 +24,39 @@ const config = {
   options: {}
 };
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#specifying_a_function_as_a_parameter
+const replacer = (_match, p1, _offset, _string) => `\"${p1}\"`;
+
+const createEventObject = (text) => {
+  try {
+    return JSON.parse(text);
+  } catch(SyntaxError) {
+    const validJSON = text
+      .replace(/'/g, '')
+      .replace(/,$/, '')
+      .replace(/([a-zA-Z_]+)/g, replacer);
+
+    return JSON.parse(validJSON);
+  }
+}
+
 window.onload = () => {
+  // Plot the chart
   new Chart(
     document.getElementById(CHART_ID),
     config
   );
+
+  // Get DOM elements
+  const eventsInput = document.getElementById(EVENTS_TEXTAREA_ID);
+  const btn = document.getElementById(BUTTON_ID);
+
+  // Process text into a list of event objects.
+  btn.addEventListener('click', () => {
+    const events = eventsInput.value;
+
+    events.split('\n').forEach(event => {
+      console.log(createEventObject(event));
+    });
+  });
 }
