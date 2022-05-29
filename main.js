539f03006d7b6675eed862cf5f140cf00e496043
Diego Vilela <vilelaphp@gmail.com>
2022-05-29 10:44:11 -0300

    Fixed bug: chart canvas must be recreated in every input

--- main.js
+++ main.js
@@ -6,11 +6,23 @@ const BUTTON_ID = 'submitButton';
 
 const labels = ['00:00', '00:01'];
 
+const renderChartCanvas = () => {
+  const oldCanvas = document.getElementById(CHART_ID);
+  if (oldCanvas) oldCanvas.remove();
+
+  const canvas = document.createElement('canvas');
+  canvas.id = CHART_ID;
+  const wrapper = document.getElementById('chartWrapper');
+  wrapper.appendChild(canvas);
+}
+
 window.onload = () => {
   const eventsSequence = document.getElementById(EVENTS_TEXTAREA_ID);
   const btn = document.getElementById(BUTTON_ID);
 
   btn.addEventListener('click', () => {
+    renderChartCanvas();
+
     plotChart(
       eventsSequence.value,
       labels,
